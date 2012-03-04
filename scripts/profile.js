caa637119f8a84b676f06bf1c228e6e65e0b22b2
Andrea Chan <an.cs.chan@gmail.com>
2012-03-04 01:57:25 -0500

    changed layout, added header

---
+++ scripts/profile.js
@@ -0,0 +1,11 @@
+	$(document).ready(function () {
+		Page_Load();
+	});
+
+
+	function Page_Load(){
+		$("#profilePicture").mouseover(function(){
+			$("#uploadPic").attr('visibility','visible');
+		});
+					
+	}
