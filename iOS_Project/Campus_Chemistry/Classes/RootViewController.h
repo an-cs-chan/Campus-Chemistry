f2ed1f115d0d0276621f0116075baac48ef40095
Trevor Sweetland <tsweet22@gmail.com>
2012-03-05 16:44:23 -0600

    Re-adding some previous changes as a part of trying to get my local repo working again.

---
+++ iOS_Project/Campus_Chemistry/Classes/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  RootViewController.h
+//  Campus_Chemistry
+//
+//  Created by Trevor Sweetland on 12-03-01.
+//  Copyright 2012 University of Manitoba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
