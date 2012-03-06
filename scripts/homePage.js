581930b95f086a9f1548e0a110898d2aa75d5b2d
= <flowacat@shaw.ca>
2012-03-05 23:53:22 -0600

    Omg. Logging in/registering is done........ dear lord.....

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -1,14 +1,14 @@
-s$(document).ready(function() { 
-
+$(document).ready(function() { 
+	
     //This function overrides the default form behavior so we can do validation
     $('#registerForm').submit(function() 
     {
         var passwordInput = $("#passwordInput").val()  
-        var passwordConfim = $("#pwConfirmInput").val();
+        var passwordConfirm = $("#pwConfirmInput").val();
         var email = $("#emailInput").val();
           
         //Passwords okay?      
-        if(passwordInput != passwordConfim)
+        if(passwordInput != passwordConfirm)
         {
             //Show error
             $("#warningArea").show("fast");
@@ -17,18 +17,17 @@ s$(document).ready(function() {
         else
         {
             //Hide error and perform normal form action
-            $("#warningArea").hide("fast");
-                      
+            $("#warningArea").hide("fast");            
+               
             //Send our POST request
             $.post(
                 "python/register.wsgi",
-                "email="+email+"&password="+passwordConfirm,
+                $("#registerForm").serialize(),
                 function(data)
                 {
-                    alert(data);
-                   //Process returned data here
-                }, "json";
-            });
+                	processRegisterRequest(data);
+                }, "json");
+        
         }
         
         return false;
@@ -41,59 +40,17 @@ s$(document).ready(function() {
         var password = $("#loginPassword").val();
        
         //Send our POST request
-        $.post({
+        $.post(
             "python/login.wsgi",
-            "email="+email+"&password="+password,
+            $("#loginForm").serialize(),
             function(data)
             {
-                alert(data);
-               //Process returned data here
-            }, "json";
-        });
-        
+				processLoginRequest(data);
+            }, "json");
+
         return false;
     });
-        
-    //select all the a tag with name equal to modal
-    $('.modalDialog').click(function(e) {
-        //Cancel the link behavior
-        e.preventDefault();
-        
-        //Get the A tag
-        var id = $(this).attr('href');
-    
-        //Get the screen height and width
-        var maskHeight = $(document).height();
-        var maskWidth = $(window).width();
-    
-        //Set heigth and width to mask to fill up the whole screen
-        $('#mask').css({'width':maskWidth,'height':maskHeight});
-        
-        //transition effect     
-        $('#mask').fadeIn(0);    
-    
-        //Get the window height and width
-        var winH = $(window).height();
-        var winW = $(window).width();
-              
-        //Set the popup window to center
-        $(id).css('top',  winH/2-$(id).height()/2);
-        $(id).css('left', winW/2-$(id).width()/2);
-    
-        //transition effect
-        $(id).fadeIn(0); 
-    
-    });
-    
-    //if close button is clicked
-    $('.window .close').click(function (e) {
-        //Cancel the link behavior
-        e.preventDefault();
-        
-        $('#mask').hide();
-        $('.window').hide();
-    });     
-    
+            
     //if mask is clicked
     $('#mask').click(function () {
         $(this).hide();
@@ -101,3 +58,29 @@ s$(document).ready(function() {
     });         
     
 });
+
+function processLoginRequest(data)
+{
+	if(data[0].status == 'Incorrect credentials')
+	{
+		alert("Wrong password!");	
+	}
+	else
+	{
+		window.location.replace("profile.html");
+	}
+}
+
+function processRegisterRequest(data)
+{
+	if(data[0].status == 'User already exists')
+	{
+        $("#warningArea").show("fast");
+        $("#warningArea").text("You already have an account! Please log in =)");		
+	}
+	else
+	{           
+		//Redirect
+		window.location.replace("profile.html");
+	}
+}
