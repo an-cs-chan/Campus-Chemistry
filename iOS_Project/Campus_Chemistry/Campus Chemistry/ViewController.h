2345ba24d4557c1f078ea9c1b8eda8c0c4fa7c18
Jae Man Lim <jaemanyayo@gmail.com>
2012-03-12 02:44:29 -0500

    This is the older version of the iOS I am doing. The new files are currently at the mac lab and forgot to attach them to my email today. I will be uploading the newer version although it is not significantly advanced from this version.

---
+++ iOS_Project/Campus_Chemistry/Campus Chemistry/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Campus Chemistry
+//
+//  Created by Jae Man Lim on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iOS_Project/Campus_Chemistry/Campus ChemistryTests/Campus_ChemistryTests.m
@@ -0,0 +1,32 @@
+//
+//  Campus_ChemistryTests.m
+//  Campus ChemistryTests
+//
+//  Created by Jae Man Lim on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Campus_ChemistryTests.h"
+
+@implementation Campus_ChemistryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Campus ChemistryTests");
+}
+
+@end
