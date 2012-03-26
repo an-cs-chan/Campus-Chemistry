a9ddb7390a4a9d4c35984c4c4115d7f8971934e4
andrea <an.cs.chan@gmail.com>
2012-03-26 04:14:12 -0500

    some refactoring and some jasmine testing

--- jasmine/spec/profileSpec.js
+++ jasmine/spec/profileSpec.js
@@ -1,29 +1,74 @@
-describe("Profile Page", function() {
+describe ("No data", function(){
 	
-	describe ("No data", function(){
-		
-		beforeEach(function () {
-	    	var data = new Array();
-	    	//setting empty values for all the 12 attributes of the profile 
-	    	for (var i=0; i<=12;i++){
-	    		data.push(" ");
-	    	}
-	    	
-			processProfileInfo(data);
-	  	});
-		
-		it("Shows no user name at top of profile", function()
-		{
-			setFixtures('<div id="user_fullname" class="homePanel"></div>');
-			expect($("#user_fullname").text()).toBe("");
-		});	
-		
-		it("Shows no user name for interests", function()
-		{
-			setFixtures('<div id="interests" class="clearfix footerPanel"></div>');
-			expect($("#interests").text()).toContain("");
-		});
+	beforeEach(function () {
+    	var data = new Array();
+    	//setting empty values for all the 12 attributes of the profile 
+    	for (var i=0; i<=12;i++){
+    		data.push(" ");
+    	}
+    	
+		processProfileInfo(data);
+  	});
 	
+	it("Shows no user name at top of profile", function()
+	{
+		setFixtures('<div id="user_fullname" class="homePanel"></div>');
+		expect($("#user_fullname").text()).toBe("");
+	});	
+	
+	it("Shows no user name for interests", function()
+	{
+		setFixtures('<div id="interests" class="clearfix footerPanel"></div>');
+		expect($("#interests").text()).toContain("");
 	});
 	
+	it("Shows no info for user gender", function()
+	{
+		setFixtures('<div id="gender" class="user_fact"></div>');
+		expect($("#gender").text()).toContain("");
+	});
+	
+	it("Shows no info for user seeking gender and age range", function()
+	{
+		setFixtures('<div id="seeking" class="user_fact"></div>');
+		expect($("#seeking").text()).toContain("");
+	});
+	
+	it("Shows no info for age", function()
+	{
+		setFixtures('<div id="age" class="user_fact"></div>');
+		expect($("#age").text()).toContain("");
+	});
+	
+	it("Shows no info for relationship status", function()
+	{
+		setFixtures('<div id="rstatus" class="user_fact"></div>');
+		expect($("#rstatus").text()).toContain("");
+	});
+	
+	it("Shows no info for ethnicity", function()
+	{
+		setFixtures('<div id="ethnicity" class="user_fact"></div>');
+		expect($("#ethnicity").text()).toContain("");
+	});
+	
+	it("Shows no info for birth country", function()
+	{
+		setFixtures('<div id="bCountry" class="user_fact"></div>');
+		expect($("#bCountry").text()).toContain("");
+	});
+	
+	it("Shows no info for faculty", function()
+	{
+		setFixtures('<div id="faculty" class="user_fact"></div>');
+		expect($("#faculty").text()).toContain("");
+	});
+	
+	it("Shows no info for department", function()
+	{
+		setFixtures('<div id="department" class="user_fact"></div>');
+		expect($("#department").text()).toContain("");
+	});
+
 });
+
--- jasmine/src/profile.js
+++ jasmine/src/profile.js
@@ -1,58 +1,58 @@
 	function processProfileInfo(data)
 	{
-		var user = data;
 		//user full name
-		$("#user_fullname").text(user[0]);
-		$("#interests").text(user[0] + "'s Interests");
+		$("#user_fullname").text(data[0]);
+		$("#interests").text(data[0] + "'s Interests");
 		
 		//gender of user
 		$("#gender").text("Gender: ");
-		if (user[1]=='F')
+		if (data[1]=='F')
 			$("#gender").append("Female");
-		else if (user[1]=='M') 
+		else if (data[1]=='M') 
 			$("#gender").append("Male");
-		else if(user[1]=="Both")
+		else if(data[1]=="Both")
 			$("#gender").append("Both");
 		
 		//gender and age user is seeking for matches
 		$("#seeking").text("Seeking: ");
-		if (user[2]=='F')
+		if (data[2]=='F')
 			$("#seeking").append("Female");
-		else if (user[2]=='M') 
+		else if (data[2]=='M') 
 			$("#seeking").append("Male");
-		else if(user[2]=="Both")
+		else if(data[2]=="Both")
 			$("#seeking").append("Both");	
-		$("#seeking").append(" (" + user[3] + "-" + user[4] +")");	
+		$("#seeking").append(" (" + data[10] + "-" + data[11] +")");	
 		
 		//age of the user
 		$("#age").text("Age: ");
-		$("#age").append(user[5]);
+		$("#age").append(data[3]);
 		
 		
 		//relationship status of user
 		$("#rstatus").text("Relationship Status: ");
-		$("#rstatus").append(user[6]);
+		$("#rstatus").append(data[4]);
 		
 		//ethnicity of user
 		$("#ethnicity").text("Ethnicity: ");
-		$("#ethnicity").append(user[7]);
+		$("#ethnicity").append(data[5]);
 		
 		//birth country of user
 		$("#bCountry").text("Country of Birth: ");
-		$("#bCountry").append(user[8]);
+		$("#bCountry").append(data[6]);
 		
 		//faculty of user
 		$("#faculty").text("Faculty: ");
-		$("#faculty").append(user[9]);
+		$("#faculty").append(data[7]);
 		
 		//department of user
 		$("#department").text("Department: ");
-		$("#department").append(user[10]);
+		$("#department").append(data[8]);
 		
+		//document.getElementById("profilePicture").src = user.photo;
 		
 		//display the interests of the user, to keep it short only 6 interests to be displayed
-		if(user.about_me!=null){
-			var interests = (user[11]).split(",");
+		if(data[9]!=null){
+			var interests = (data[9]).split(",");
 		}
 		
 		var i=0;
