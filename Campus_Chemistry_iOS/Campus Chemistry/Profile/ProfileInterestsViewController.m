0e7cbfb95751e9d3fc973a436058c1974e831eb0
Trevor Sweetland <tsweet22@gmail.com>
2012-03-25 23:53:22 -0500

    got the profile page display working yay

--- Campus_Chemistry_iOS/Campus Chemistry/Profile/ProfileInterestsViewController.m
+++ Campus_Chemistry_iOS/Campus Chemistry/Profile/ProfileInterestsViewController.m
@@ -7,15 +7,21 @@
 //
 
 #import "ProfileInterestsViewController.h"
+#import "EditInterestsViewController.h"
+#import "SBJson.h"
 
 @implementation ProfileInterestsViewController
 
+@synthesize editInterests;
+
+@synthesize lblusername, lblInterest1, lblInterest2, lblInterest3, lblInterest4, lblInterest5, lblInterest6;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.title = NSLocalizedString(@"Interests", @"Interests");
     }
     return self;
 }
@@ -34,6 +40,56 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.navigationItem.title = @"Profile Interests";
+    
+    //get the user id
+    //returnUser = (AppDelegate*)[[UIApplication sharedApplication] delegate];
+    //NSString *email = [returnUser getUserEmail];
+    NSString *email=@"an.cs.chan@gmail.com";
+    NSString *args = [NSString stringWithFormat:@"userid=%@", email];
+    
+    NSString *msgLength = [NSString stringWithFormat:@"@d", [args length]];
+    NSURL *url = [NSURL URLWithString:@"http://ec2-107-22-123-18.compute-1.amazonaws.com/python/profile.wsgi"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60.0];
+    [request addValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-type"];
+    [request addValue:msgLength forHTTPHeaderField:@"Content-Length" ];
+    [request setHTTPMethod:@"POST"];   
+    [request setHTTPBody:[args dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSURLResponse* response;
+    NSError* error = nil;
+    
+    //Capturing server response
+    NSData* result = [NSURLConnection sendSynchronousRequest:request  returningResponse:&response error:&error];   
+    NSString *resultString = [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];    
+    
+    SBJsonParser *jsonParser = [[SBJsonParser alloc] init];
+    NSArray *jsonObjects = [jsonParser objectWithString:resultString error:&error];
+    
+    profiles = [[NSMutableArray alloc] init];
+    
+    for (NSDictionary *dict in jsonObjects)
+    {
+        Profile *profile = [[Profile alloc] init];
+        [profile setName:[dict objectForKey:@"name"]];
+        [profile setInterests:[dict objectForKey:@"about_me"]];
+        
+        [profiles addObject:profile];
+    }
+    
+    //set label for user name
+    lblusername.text = [NSString stringWithFormat: @"%@'s Interests", [[profiles objectAtIndex:0] name]];
+    
+    //parse interests and set labels for interests
+    array = [[NSMutableArray alloc] init];
+    array = [[[[profiles objectAtIndex:0] interests] componentsSeparatedByString:@","] mutableCopy];
+    
+    lblInterest1.text = [NSString stringWithFormat: @"- %@", [array objectAtIndex:0]];
+    lblInterest2.text = [NSString stringWithFormat: @"- %@", [array objectAtIndex:1]];
+    lblInterest3.text = [NSString stringWithFormat: @"- %@", [array objectAtIndex:2]];
+    lblInterest4.text = [NSString stringWithFormat: @"- %@", [array objectAtIndex:3]];
+    lblInterest5.text = [NSString stringWithFormat: @"- %@", [array objectAtIndex:4]];
+    lblInterest6.text = [NSString stringWithFormat: @"- %@", [array objectAtIndex:5]];
 }
 
 - (void)viewDidUnload
--- Campus_Chemistry_iOS/Campus Chemistry/Profile/ProfilePhotoViewController.m
+++ Campus_Chemistry_iOS/Campus Chemistry/Profile/ProfilePhotoViewController.m
@@ -7,14 +7,18 @@
 //
 
 #import "ProfilePhotoViewController.h"
+#import "EditPhotoViewController.h"
+#import "SBJson.h"
 
 @implementation ProfilePhotoViewController
+@synthesize editPhoto;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.title = NSLocalizedString(@"Photo", @"Photo");
     }
     return self;
 }
@@ -33,6 +37,55 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.navigationItem.title = @"Profile Photo";
+    
+    //get the user id
+    //returnUser = (AppDelegate*)[[UIApplication sharedApplication] delegate];
+    //NSString *email = [returnUser getUserEmail];
+    NSString *email=@"an.cs.chan@gmail.com";
+    NSString *args = [NSString stringWithFormat:@"userid=%@", email];
+     
+    
+    NSString *msgLength = [NSString stringWithFormat:@"@d", [args length]];
+    NSURL *url = [NSURL URLWithString:@"http://ec2-107-22-123-18.compute-1.amazonaws.com/python/profile.wsgi"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:60.0];
+    [request addValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-type"];
+    [request addValue:msgLength forHTTPHeaderField:@"Content-Length" ];
+    [request setHTTPMethod:@"POST"];   
+    [request setHTTPBody:[args dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSURLResponse* response;
+    NSError* error = nil;
+    
+    //Capturing server response
+    NSData* result = [NSURLConnection sendSynchronousRequest:request  returningResponse:&response error:&error];   
+    NSString *resultString = [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];    
+    
+    SBJsonParser *jsonParser = [[SBJsonParser alloc] init];
+    NSArray *jsonObjects = [jsonParser objectWithString:resultString error:&error];
+    
+    profiles = [[NSMutableArray alloc] init];
+    
+    for (NSDictionary *dict in jsonObjects)
+    {
+        Profile *profile = [[Profile alloc] init];
+        
+        [profile setPhoto:[dict objectForKey:@"photo"]];
+        //NSLog(@"photo:%@", [dict objectForKey:@"photo"]);
+        [profiles addObject:profile];
+    }
+    
+    //IT WONT TAKE AN EXTERNAL IMAGE :((((
+    //lbluserName.text = [[profiles objectAtIndex:0] photo];
+    //NSImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:MyURL]]];
+    //NSLog([[profiles objectAtIndex:0] photo]]); 
+    //UIImage * img = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://example.com/image.png"]]];
+    //UIImage *img = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:[[profiles objectAtIndex:0] photo]]]];
+    //profilePhoto.image = img;
+    /*NSURL *imageURL = [NSURL URLWithString: [[profiles objectAtIndex:0] photo]];
+    NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+    UIImage *image = [UIImage imageWithData:imageData]; 
+    profilePhoto.image = image;*/
 }
 
 - (void)viewDidUnload
